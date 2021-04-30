e6c5c7bef14c506ac2f8418fec9c6c97374b4325
Marquell Thompson <mqt060@gmial.com>
2021-04-30 09:22:00 -0500

    adding view all employees function

--- index.js
+++ index.js
@@ -14,16 +14,24 @@ const server = mysql.createConnection({
 
 })
 // startProgram()
-function startProgram(){
+function startProgram() {
     inquirer.prompt([
         {
-            type:'list',
-            message:'What would you like to do?',
-            choices:['Veiw All Employee','Veiw All Employees by Department','Veiw All Employees by Manager','Add Employees','Remove Employee','Update Employee Role','Update Employee Manager'],
-            name:'startQ'
-    }
-]).then((response) => {
-    
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: ['Veiw All Employee', 'Veiw All Employees by Department', 'Veiw All Employees by Manager', 'Add Employees', 'Remove Employee', 'Update Employee Role', 'Update Employee Manager'],
+            name: 'startQ'
+        }
+    ]).then((response) => {
+    })
+}
+function viewAllEmployees() {
 
-})
+    let query = 'select employee_ID,first_name,last_name,role_TITLE,department_NAME,role_SALERY from employees inner join roles on employees.role_ID = roles.role_ID inner join departments on departments.department_ID = roles.department_ID; ';
+
+    server.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res)
+    })
+
 }
